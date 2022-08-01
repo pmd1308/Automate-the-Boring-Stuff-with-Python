14aefcb5615feb98e3c13788075dfc00fb1b08c2
Pedro <dx1308@pm.me>
2022-08-01 19:53:11 -0300

    Aula 1

---
+++ Writing_Our_First_Program.py
@@ -0,0 +1,9 @@
+print("Hello World")
+print("Whats your name?")
+myName = input()
+print("It's good to meet you," + myName)
+print("The lenght of your name is:")
+print(len(myName))
+print("What's your age?")
+myAge = input
+print("You will be" + str(int(myAge)+1) + "in a year")
